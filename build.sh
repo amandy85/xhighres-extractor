3339cad807a06b7f7fbb34e877c52318f61098a6
aman <amandeepyadav85630@gmail.com>
2025-08-01 23:16:34 +0530

    Fix ChromeDriver download: use dynamic version matching

--- build.sh
+++ build.sh
@@ -1,22 +1,13 @@
-#!/bin/bash
-set -o errexit
+# Install Chrome first
+sudo dpkg -i google-chrome-stable_current_amd64.deb || sudo apt-get install -fy
 
-# Install Chrome
-mkdir -p /opt/render/project/src/.render/chrome
-cd /opt/render/project/src/.render/chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-ar x google-chrome-stable_current_amd64.deb data.tar.xz
-tar -xf data.tar.xz
-rm google-chrome-stable_current_amd64.deb data.tar.xz
-chmod +x /opt/render/project/src/.render/chrome/opt/google/chrome/google-chrome
+# Get installed Chrome version (output: "Google Chrome 138.0.7080.0")
+CHROME_VERSION=$(google-chrome --version)
 
-# Install specific ChromeDriver version (compatible with Chrome 138)
-CHROMEDRIVER_VERSION="138.0.0"
-wget https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip
-chmod +x chromedriver
-mv chromedriver /usr/local/bin/
+# Extract the major version number (e.g., 138)
+CHROME_MAJOR=${CHROME_VERSION#* }  # Remove "Google Chrome"
+CHROME_MAJOR=${CHROME_MAJOR%%.*}   # Take only first number
 
-# Install Python dependencies
-cd /opt/render/project/src
-pip install -r requirements.txt
+# Fetch matching ChromeDriver version
+CHROMEDRIVER_VERSION=$(wget -qO- "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_MAJOR")
+wget "https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip"
